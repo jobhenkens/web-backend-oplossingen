641150aece156157b772eb89f8cff3c587cd9a0e
Job Henkens <jobhenkens@MacBook-Pro-van-Job-Henkens-2.local>
2015-08-14 21:04:08 +0200

    map string-concatenate toegevoegd

---
+++ string-concatenate/opdracht-string-concatenate.php
@@ -0,0 +1,31 @@
+<?php
+
+    $voornaam = 'Job';
+    $naam = 'Henkens';
+    $volledigeNaam = $voornaam . ' ' . $naam;
+    $len = strlen( $volledigeNaam );
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht string concatenate</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+        
+    		<h1>Opdracht string concatenate: deel 1</h1>
+            <p>Dit is mijn naam: <?= $volledigeNaam ?></p>
+            <p><?= $len ?></p>
+
+        </section>
+
+    </body>
+</html>
